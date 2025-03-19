9cb4b716e37eacc96dcf12ab738fa53699af3e52
Nedal <ibrahimanedal@gmail.com>
2025-03-19 07:27:42 -0400

    Update route.ts

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -3,5 +3,6 @@ import { handleLogin } from '@auth0/nextjs-auth0';
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const returnTo = url.searchParams.get('returnTo') || '/dashboard';
+  // @ts-ignore
   return handleLogin(request, { returnTo });
 }
