bfeb99e3c5463ccd94724d062141d9e18e4ce97a
Nedal <ibrahimanedal@gmail.com>
2025-03-19 07:28:15 -0400

    Update route.ts

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -2,5 +2,6 @@ import { handleProfile } from '@auth0/nextjs-auth0/edge';
 
 export async function GET(request: Request) {
     // Force no caching
+    // @ts-ignore
     return handleProfile(request, { cache: 'no-store' });
 }
