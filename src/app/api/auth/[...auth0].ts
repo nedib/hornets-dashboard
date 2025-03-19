323448f70f73e875c459fdd03b5d2c825c923f2b
Nedal <ibrahimanedal@gmail.com>
2025-03-19 05:52:24 -0400

    Update [...auth0].ts

--- src/app/api/auth/[...auth0].ts
+++ src/app/api/auth/[...auth0].ts
@@ -1,6 +1,10 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
+if (!process.env.AUTH0_SECRET) {
+  throw new Error("Missing AUTH0_SECRET environment variable");
+}
+
 export default handleAuth({
-    cookieSecret: process.env.AUTH0_SECRET,
-    cookieSecure: process.env.NODE_ENV === 'production',
+  cookieSecret: process.env.AUTH0_SECRET,
+  cookieSecure: process.env.NODE_ENV === 'production',
 });
