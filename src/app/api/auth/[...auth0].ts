732226fe11fee3566aa4508b7b031e2b73fd6e0d
Nedal <ibrahimanedal@gmail.com>
2025-03-19 06:11:23 -0400

    Update [...auth0].ts

--- src/app/api/auth/[...auth0].ts
+++ src/app/api/auth/[...auth0].ts
@@ -1,6 +1,3 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export default handleAuth({
-  cookieSecret: process.env.AUTH0_SECRET!, 
-  cookieSecure: process.env.NODE_ENV === 'production',
-});
+export default handleAuth();
