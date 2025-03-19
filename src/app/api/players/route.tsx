460405005e14b4b899e1658f6342147c2b22702c
Nedal <ibrahimanedal@gmail.com>
2025-03-19 05:31:10 -0400

    Update route.tsx

--- src/app/api/players/route.tsx
+++ src/app/api/players/route.tsx
@@ -4,7 +4,7 @@ export async function GET() {
     //just some  data
     const players = [
         {
-            name: " Player 1",
+            name: "Mark Williams",
             pointsPerGame: 22.5,
             rebounds: 7.8,
             assists: 5.1,
@@ -12,7 +12,7 @@ export async function GET() {
             minutes: 32,
         },
         {
-            name: " Player 2",
+            name: "Grant Williams",
             pointsPerGame: 18.3,
             rebounds: 6.5,
             assists: 4.2,
@@ -20,7 +20,7 @@ export async function GET() {
             minutes: 30,
         },
         {
-            name: "Player 3",
+            name: "Marcus Garrett",
             pointsPerGame: 15.6,
             rebounds: 4.3,
             assists: 3.8,
@@ -28,7 +28,7 @@ export async function GET() {
             minutes: 28,
         },
         {
-            name: "Player 4",
+            name: "LaMelo Ball",
             pointsPerGame: 12.9,
             rebounds: 5.1,
             assists: 2.7,
@@ -36,7 +36,7 @@ export async function GET() {
             minutes: 26,
         },
         {
-            name: "Player 5",
+            name: "Miles Bridges",
             pointsPerGame: 10.4,
             rebounds: 3.9,
             assists: 2.1,
