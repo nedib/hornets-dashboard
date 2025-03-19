ba1e232bc6d0b344aaefa8cf720f8b481290e201
Nedal <ibrahimanedal@gmail.com>
2025-03-19 07:26:53 -0400

    Update route.ts

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -2,5 +2,6 @@ import { handleCallback } from '@auth0/nextjs-auth0/edge';
 import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
+    // @ts-ignore
     return handleCallback(request);
 }
